a56bee0e9b03b8b0643ed0f2a123759ba8c461e2
mjjcha <mjjcha@gmail.com>
2017-07-21 04:19:21 -0700

    Minor updates

--- message.go
+++ message.go
@@ -4,4 +4,7 @@ type Message struct {
 	FromUsername string `json:"from_username"`
 	ToUser       string `json:"to_username"`
 	Payload      string `json:"payload"`
+	Email    		 string `json:"email"`
+	Username 		 string `json:"username"`
+	Message  		 string `json:"message"`
 }
